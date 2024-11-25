cb390ae18b48d17a419ce7e1fc9f629c09ec42f0
Septian Abiyoga <septianyoga111@gmail.com>
2024-11-25 20:35:58 +0700

    update: scoring test and add test 13

--- resources/views/user/test-page-result.blade.php
+++ resources/views/user/test-page-result.blade.php
@@ -1,6 +1,6 @@
 @extends('user.components.base', ['title' => 'Test Result'])
 @section('content')
-    <section id="payment-success-first" style="background-color: #F90965;">
+    <section id="payment-success-first" class="{{ $test->score >= 12 ? 'bg-red' : 'bg-green' }}">
         <div class="payment-success-first-wrap">
             <img src="{{ asset('assets/images/payment-success/bg-img.png') }}" class="img-fluid" alt="bg-img">
             <div class="payment-logo-sec">
@@ -12,11 +12,13 @@
                 <div class="payment-success-second">
                     <div class="container">
                         <div class="payment-success-second-wrap">
-                            <h1 class="success-title">19<span>pt</span></h1>
-                            <h1 class="success-title-risk">High Risk!</h1>
+                            <h1 class="success-title">{{ $test->score }}<span>pt</span></h1>
+                            <h1 class="success-title-risk"> {{ $test->score >= 12 ? 'High Risk' : 'Low Risk' }}!</h1>
                             <div class="success-img mt-24">
                             </div>
-                            <p class="success-para">Resiko tinggi. Segera dilakukan perawatan di Rumah Sakit.</p>
+                            <p class="success-para">
+                                {{ $test->score >= 12 ? 'Resiko tinggi. Segera dilakukan perawatan di Rumah Sakit.' : 'Resiko rendah. Tidak perlu dilakukan perawatan di Rumah Sakit.' }}
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -27,4 +29,15 @@
             <a href="{{ route('test-page') }}" class="test-again">Test Ulang</a>
         </div>
     </section>
+@endsection
+@section('css')
+    <style>
+        .bg-green {
+            background-color: #00930c !important;
+        }
+
+        .bg-red {
+            background-color: #F90965 !important;
+        }
+    </style>
 @endsection
